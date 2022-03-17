c0798e0814471226a0a12b8103256ec3b281a6ee
VDEV <vitorfeliperamosmello@edu.univali.br>
2022-03-17 20:04:49 -0300

    Updated all resources

--- GanjGameEngine/Source.cpp
+++ GanjGameEngine/Source.cpp
@@ -4,6 +4,7 @@
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "DirectXTK.lib")
 
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR    lpCmdLine,
