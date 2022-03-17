9da25a1a15a1f68ac30580b04570fcd81dfbc920
VDEV <vitorfeliperamosmello@edu.univali.br>
2022-03-17 19:30:49 -0300

    Add a includes and libs in script and .sln
    Initial configuration to project

--- GanjGameEngine/Source.cpp
+++ GanjGameEngine/Source.cpp
@@ -1,8 +1,8 @@
 //Tutorial 1 Solution 2018-09-07 
 
 #include <Windows.h>
-#pragma comment(lib,"d3d11.lib")
-#pragma comment(lib,"DirectXTK.lib")
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "DirectXTK.lib")
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
