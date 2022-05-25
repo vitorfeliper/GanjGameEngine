6042142100e5c085916ecb8fa085f67de3621637
VDEV <vitorfeliperamosmello@edu.univali.br>
2022-05-25 11:51:02 -0300

    Updated Total
    Remove test versions

--- GanjGameEngineJAVA/PROJECT/src/main/java/Util/Time.java
+++
@@ -1,8 +0,0 @@
-package Util;
-
-public class Time {
-    public static float timeStarted = System.nanoTime();
-    public static float deltaTime(){ return (float)((System.nanoTime() - timeStarted) * 1E-9); }
-}
-
-
