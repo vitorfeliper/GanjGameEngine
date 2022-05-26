5549f9081d2c3aee23d73c3222dda9c5ae1165d5
VDEV <vitorfeliperamosmello@edu.univali.br>
2022-05-26 14:46:52 -0300

    New VK Engine

--- engine/build.bat
+++ engine/build.bat
@@ -1,10 +1,10 @@
 REM Build script for engine
 @ECHO OFF
-setLocal EnableDelayedExpansion
+SetLocal EnableDelayedExpansion
 
 REM Get a list of all the .c files.
-SET cFilenames = 
-FOR /R %%f in (*.c) do(
+SET cFilenames=
+FOR /R %%f in (*.c) do (
     SET cFilenames=!cFilenames! %%f
 )
 
@@ -14,7 +14,7 @@ SET assembly=engine
 SET compilerFlags=-g -shared -Wvarargs -Wall -Werror
 REM -Wall -Werror
 SET includeFlags=-Isrc -I%VULKAN_SDK%/Include
-SET linkerFlags=- luser32 -lvulkan-1 -L%VULKAN_SDK%/Lib
+SET linkerFlags=-luser32 -lvulkan-1 -L%VULKAN_SDK%/Lib
 SET defines=-D_DEBUG -DKEXPORT -D_CRT_SECURE_NO_WARNINGS
 
 ECHO "Building %assembly%%..."
---
+++ testbed/src/main.c
@@ -0,0 +1,6 @@
+#include <test.h>
+
+int main() {
+    print_int(1);
+    return 0;
+}
