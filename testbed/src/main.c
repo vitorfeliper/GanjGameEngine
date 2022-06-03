05bb08f0ae684aed8319a25a8cfb566cf2aa0774
VDEV <vitorfeliperamosmello@edu.univali.br>
2022-06-02 21:05:50 -0300

    Platform Layer
    Window platform and Window programing added
    Logger logic optmized

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,15 +1,24 @@
 #include <core/logger.h>
 #include <core/asserts.h>
 
-int main(void) {
-    KFATAL("A test message: %f", 3.14f);
-    KERROR("A test message: %f", 3.14f);
-    KWARN("A test message: %f", 3.14f);
-    KINFO("A test message: %f", 3.14f);
-    KDEBUG("A test message: %f", 3.14f);
-    KTRACE("A test message: %f", 3.14f);
+// TODO: Test
+#include <platform/platform.h>
 
-    KASSERT(1 == 0);
+int main(void) {
+    KFATAL("\nA test message: %f", 3.14f);
+    KERROR("\nA test message: %f", 3.14f);
+    KWARN("\nA test message: %f", 3.14f);
+    KINFO("\nA test message: %f", 3.14f);
+    KDEBUG("\nA test message: %f", 3.14f);
+    KTRACE("\nA test message: %f", 3.14f);
+
+    platform_state state;
+    if(platform_startup(&state, "GanjGame Engine TestBed", 100, 100, 640, 480)){
+        while(TRUE){
+            platform_pump_messages(&state);
+        }
+    }
+    platform_shutdown(&state);
 
     return 0;
 }
